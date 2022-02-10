9890d0de440fd26f94ff9adedfcce3b0e8898361
Florian Dupuis <floriandupuis1996@gmail.com>
2022-02-10 07:59:48 -0500

    first commit

---
+++ components/Base/Button.vue
@@ -0,0 +1,9 @@
+<template>
+  <button class="btn btn-primary mt-1"><slot /></button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <Header />
+    <BaseTitle>Hello Word</BaseTitle>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet delectus
+      fuga aliquid, accusantium laboriosam, doloribus dolore illum, deserunt
+      ipsam quisquam ad vel laborum ab facere non eligendi! Vel, quia alias.
+      Blanditiis, explicabo id cumque iste nesciunt, rem eius, laudantium ex
+      neque aut et molestiae dolor dicta deserunt deleniti!
+    </p>
+    <BaseButton>Hello</BaseButton>
+    <Footer />
+  </main>
+</template>
+
+<script>
+import Footer from "../components/Footer.vue";
+export default {
+  components: { Footer },
+  name: "IndexPage",
+};
+</script>
